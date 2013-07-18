e37c2b90c892b881356c66ddcd5c423b01dc3fba
Ralph <ralphoouu@gmail.com>
2013-07-18 14:02:14 -0400

    DB Access interface

---
+++ src/Database/DBAccess.java
@@ -0,0 +1,13 @@
+package Database;
+
+/**
+ * External users of this package should use this interface to make calls to the
+ * database. No arbitrary SQL should ever be executed
+ * @author Ralph
+ *
+ */
+public interface DBAccess {
+
+	
+	
+}
---
+++ src/Database/DBAccessImpl.java
@@ -0,0 +1,12 @@
+package Database;
+
+/**
+ * A simple (non-hibernate based) implementation of the database interface.
+ * @author Ralph
+ *
+ */
+public class DBAccessImpl implements DBAccess {
+	
+	
+
+}
