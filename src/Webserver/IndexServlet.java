63209b184f13b68581d4f02a2493a070f44ec1aa
Ralph <ralphoouu@gmail.com>
2013-07-18 11:22:50 -0400

    Tweaks

--- src/Webserver/IndexServlet.java
+++ src/Webserver/IndexServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class IndexServlet
  */
-@WebServlet("/IndexServlet")
+@WebServlet("/index")
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
